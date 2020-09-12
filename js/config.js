de42ef4578e8d7a326d84995510576fb414055ea
Tatsuya Koyama <tatsuyakoyama@TatsuyanoMacBook-Pro.local>
2020-09-12 16:04:46 +0900

    debug

--- js/config.js
+++ js/config.js
@@ -12,8 +12,8 @@ $(function(){
 	// 	$("body").addClass('night');
     // });
     
-    // key = "aaa";
-    // val = '{"morning": ["aaa","bbb","ccc"],"night": []}';
+    // key = "morning";
+    // val = '{"urls":["aaa","bbb","ccc"]}';
 
     // //保存
     // localStorage.setItem(key, val);
@@ -26,15 +26,92 @@ $(function(){
     //     alert(elem);
     //   }
    
-
+    //
+    var idIncrement = 1;
+    var type = getParam("type");
+    var listByType = localStorage.getItem(type);
+    if(listByType){
+        var objects  = JSON.parse(listByType);
+        for (const obj of objects.urls) {
+            addForm(obj);
+        }    
+    }
+    $(document).on("click", ".deleteButton", function () {
+    // $('.deleteButton').on('click', function() {
+        var deleteId = $(this).data("did");
+        $("#"+deleteId).remove();
+    });
     $('#addButton').on('click', function() {
-        $('#form_area').append('<input type="text">');
-      });
-    // var input_data = document.createElement('input');
-    // input_data.type = 'text';
-    // input_data.id = 'inputform_' + i;
-    // input_data.placeholder = 'フォーム-' + i;
-    // var parent = document.getElementById('form_area');
-    // parent.appendChild(input_data);
-    // i++ ;
+        addForm("");
+    });
+    $('#save').on('click', function() {
+
+        var urlsData = Array();
+        $('.url').each(function(){
+            urlsData.push($(this).val()) ;
+        });
+
+        var jsonArray = {
+            urls: urlsData
+        }
+        let complexDataJSON = JSON.stringify(jsonArray);
+        localStorage.setItem(type, complexDataJSON);
+    });
+    //テキストボックスのフォーカスが外れたら発動
+    $(document).on('blur', 'input[type="text"]', function() {
+        var inputId = $(this).data("iid");
+        var inputUrl = $(this).val();
+ 
+        $.ajax({
+            url:inputUrl,
+            type: 'GET',
+            cache: false,
+            dataType: 'html'
+          }).done(function(html) {
+            var t = html.match(/<title>(.*)<\/title>/);
+            var title = t[1];
+            $("#"+inputId).find(".title").text(title);
+          }).fail(function() {
+            alert('エラーが起きました');
+          }).always(function() {
+            console.log('complete');
+          });
+          
+    });
+    function addForm(url){
+        idIncrement++;
+        var id = "did_"+idIncrement;
+        var title = getUrlTitle(url);
+        $('#form_area').append('<div id="'+id+'"><div class="title">'+title+'</div><input data-iid="'+id+'" type="text" value="'+url+'" class="url" name="url[]"><div class="deleteButton" data-did="'+id+'">Delete</div></div>');
+    }
+    
+    function getUrlTitle(url){
+        //key = "url"+window.btoa(url);
+        key="aaa";
+        title = localStorage.getItem(key);
+        if(title){
+            return title;
+        }
+        return "";
+
+    }
+    function setUrlTitle(url,title){
+        //key = "url"+window.btoa(url);
+        key="aaa";
+
+        alert(key + "_" + title)
+        localStorage.setItem(key, title);
+
+    }
+
+
+    function getParam(name, url) {
+        if (!url) url = window.location.href;
+        name = name.replace(/[\[\]]/g, "\\$&");
+        var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+            results = regex.exec(url);
+        if (!results) return null;
+        if (!results[2]) return '';
+        return decodeURIComponent(results[2].replace(/\+/g, " "));
+    }
 });
